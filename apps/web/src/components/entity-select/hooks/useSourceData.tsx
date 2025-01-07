23911513cdf41f096de979b13d7c7929344ae2b3
Jeffrey <zhuangjh@milesight.com>
2025-01-07 11:10:16 +0800

    feat: Entity select component interface request to add cache

--- apps/web/src/components/entity-select/hooks/useSourceData.tsx
+++ apps/web/src/components/entity-select/hooks/useSourceData.tsx
@@ -1,86 +1,47 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useShallow } from 'zustand/react/shallow';
-import { useMemoizedFn, useRequest } from 'ahooks';
+import { useInitialize } from './useInitialize';
+import { useFilter } from './useFilter';
+import { useSearch } from './useSearch';
 import useEntityStore from '../store';
-import type { EntitySelectProps } from '../types';
+import type { FilterParameters } from '../types';
 
-export const useSourceData = (
-    props: Pick<
-        EntitySelectProps,
-        'entityType' | 'entityAccessMod' | 'excludeChildren' | 'entityValueType'
-    >,
-) => {
+export const useSourceData = (props: FilterParameters) => {
     const { entityType, entityAccessMod, excludeChildren, entityValueType } = props;
-    const [keyword, setKeyword] = useState('');
-    const { entityList, getEntityList, entityLoading, initEntityList, status } = useEntityStore(
+    const { entityList, entityLoading } = useEntityStore(
         useShallow(state => ({
             entityList: state.entityList,
             entityLoading: state.entityLoading,
-            getEntityList: state.getEntityList,
-            initEntityList: state.initEntityList,
-            status: state.status,
         })),
     );
-
-    const init = useMemoizedFn(() => {
-        initEntityList();
-    });
-    useEffect(() => {
-        if (status !== 'ready') return;
-
-        init();
-    }, [init, initEntityList, status]);
-
-    const { data: searchEntityList, loading: searchLoading } = useRequest(
-        async () => {
-            if (!keyword) return [];
-
-            const params = {
-                keyword,
-                entityType,
-                entityAccessMod,
-                excludeChildren,
-                entityValueType,
-            };
-            return getEntityList(params);
-        },
-        { refreshDeps: [keyword], debounceWait: 300 },
-    );
-    /** search entity list by keyword */
-    const onSearch = useCallback((keyword: string) => {
-        setKeyword(keyword);
-    }, []);
 
     const hasFilterParams = useMemo(
         () => !!(entityType || entityAccessMod || excludeChildren || entityValueType),
         [entityType, entityAccessMod, excludeChildren, entityValueType],
     );
-    const { data: sourceEntityList, loading: sourceSearchLoading } = useRequest(
-        async () => {
-            if (!hasFilterParams) return;
-
-            const params = {
-                entityType,
-                entityAccessMod,
-                excludeChildren,
-                entityValueType,
-            };
-            return getEntityList(params);
-        },
-        {
-            refreshDeps: [entityType, entityAccessMod, excludeChildren, entityValueType],
-        },
-    );
+    useInitialize({ hasFilterParams });
+    const { filterLoading, filterEntityList } = useFilter({
+        entityType,
+        entityAccessMod,
+        excludeChildren,
+        entityValueType,
+        hasFilterParams,
+    });
+    const { keyword, searchEntityList, searchLoading, onSearch } = useSearch({
+        entityType,
+        entityAccessMod,
+        excludeChildren,
+        entityValueType,
+    });
 
     const fetchEntityList = useMemo(() => {
-        if (!keyword) return hasFilterParams ? sourceEntityList : entityList;
+        if (!keyword) return hasFilterParams ? filterEntityList : entityList;
         return searchEntityList;
-    }, [entityList, hasFilterParams, keyword, searchEntityList, sourceEntityList]);
+    }, [entityList, hasFilterParams, keyword, searchEntityList, filterEntityList]);
     const fetchLoading = useMemo(() => {
-        if (!keyword) return hasFilterParams ? sourceSearchLoading : entityLoading;
+        if (!keyword) return hasFilterParams ? filterLoading : entityLoading;
         return searchLoading;
-    }, [entityLoading, hasFilterParams, keyword, searchLoading, sourceSearchLoading]);
-
+    }, [entityLoading, hasFilterParams, keyword, searchLoading, filterLoading]);
     return {
         entityList: useMemo(() => fetchEntityList || [], [fetchEntityList]),
         loading: fetchLoading,
