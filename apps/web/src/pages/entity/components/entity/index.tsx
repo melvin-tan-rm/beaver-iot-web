874f8a6282bb069dc633d08252945341bb84a609
kangyiban <kangyb@milesight.com>
2025-05-07 09:43:37 +0800

    fix: Modify the file name format of the entity data export

--- apps/web/src/pages/entity/components/entity/index.tsx
+++ apps/web/src/pages/entity/components/entity/index.tsx
@@ -10,8 +10,8 @@ import {
     FilterValue,
     TableProProps,
 } from '@/components';
-import { useI18n } from '@milesight/shared/src/hooks';
-import { objectToCamelCase, xhrDownload } from '@milesight/shared/src/utils/tools';
+import { useI18n, useTime } from '@milesight/shared/src/hooks';
+import { genRandomString, objectToCamelCase, xhrDownload } from '@milesight/shared/src/utils/tools';
 import { getCurrentComponentLang } from '@milesight/shared/src/services/i18n';
 import { getAuthorizationToken } from '@milesight/shared/src/utils/request/utils';
 import { IosShareIcon, toast } from '@milesight/shared/src/components';
@@ -36,6 +36,7 @@ import ExportModal from '../export-modal';
 export default () => {
     const navigate = useNavigate();
     const { getIntlText } = useI18n();
+    const { getTimeFormat, dayjs } = useTime();
     const { hasPermission } = useUserPermissions();
 
     const [keyword, setKeyword] = useState<string>();
@@ -123,7 +124,10 @@ export default () => {
         }
         xhrDownload({
             assets: url,
-            fileName: `EntityData_${Date.now()}.csv`,
+            fileName: `EntityData_${getTimeFormat(dayjs(), 'simpleDateFormat').replace(/-/g, '_')}_${genRandomString(
+                6,
+                { upperCase: false, lowerCase: true, number: true },
+            )}.csv`,
             header: {
                 'Accept-Language': getCurrentComponentLang(),
                 Authorization: getAuthorizationToken(),
