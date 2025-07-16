af9666b2417f8496d1a22820fee94ca82e259428
jimco <xiejc@milesight.com>
2025-07-16 10:53:13 +0800

    fix: Retain an div tag and fix the `insertBefore` error during browser translation

--- packages/shared/src/components/loading-button/index.tsx
+++ packages/shared/src/components/loading-button/index.tsx
@@ -12,7 +12,9 @@ const LoadingButton: React.FC<
 > = ({ children, loading, disabled, indicatorColor = 'inherit', ...otherProps }) => {
     return (
         <Button disabled={disabled || loading === true || false} {...otherProps}>
-            {loading && <CircularProgress size={16} color={indicatorColor} sx={{ mr: 1 }} />}
+            <div className="ms-loading-icon">
+                {loading && <CircularProgress size={16} color={indicatorColor} sx={{ mr: 1 }} />}
+            </div>
             {children}
         </Button>
     );
